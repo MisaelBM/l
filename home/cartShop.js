35c285deffd959ee4704a7d8727fc2395f902c1a
Misael Morgado <112130147+MisaelBM@users.noreply.github.com>
2023-04-28 17:07:36 -0300

    Update cartShop.js

--- home/cartShop.js
+++ home/cartShop.js
@@ -61,7 +61,6 @@ let addCartClick = document.querySelectorAll(".addCartClick").forEach(add => add
     arrMemoryCart = [];
     AlertCartFunction();
      ViewCartContentFunction();
-}))
 
 const cartView = document.getElementById("cartView");
 const cartVisor = document.getElementById("cartVisor");
