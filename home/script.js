36a4fa59fd5729d57ac5b0ff6fc9ea549ad66d07
misaelBM <morgadoandreia84misael@gmail.com>
2023-04-28 16:11:14 -0300

    l

--- home/script.js
+++ home/script.js
@@ -35,3 +35,59 @@ setInterval (
     }
 ,6000);
 
+const productsViewInfo = document.getElementById("productsViewInfo");
+const menuSmallView = document.getElementById("menuSmallView");
+const barPrincipal = document.getElementById("barPrincipal");
+const blurBackground = document.getElementById("blurBackground");
+const contentSalesProducts = document.getElementById("contentSales");
+let productClick = document.querySelectorAll(".productClick").forEach(e => {
+    e.addEventListener("click", function () {
+        barPrincipal.style.display = "none";
+        menuSmallView.style.display = "none";
+        productsViewInfo.style.display = "block";
+        contentSalesProducts.style.filter = "blur(8px)";
+        blurBackground.style.display = "block";
+        let imgProductInfo = e.querySelector("img");
+        imgProductInfo = imgProductInfo.getAttribute("src");
+        let textProductInfo = e.textContent;
+        textProductInfo = textProductInfo.split("R$");
+        productsViewInfo.innerHTML = `
+            <i class="fa-solid fa-square-xmark fa-xl exit-info-product" id="clickExitProduct"></i>
+            <img src="${imgProductInfo}" alt="" class="img-product-info">
+            <div class="info-product-text">
+                <h1>${textProductInfo[0]}</h1>
+                <h2 class="price">R$${textProductInfo[1]}</h2>
+                <hr>
+                <div class="amount-products" id="amountProducts">
+                    <div class="buttons-amount"><button id="plusProducts" class="plus-products"><i class="fa-solid fa-plus fa-sm"></i></button><input type="number" value="1" class="view-amount" id="viewAmount" min="1"><button class="minus-products" id="minusProducts"><i class="fa-solid fa-minus fa-sm"></i></button></div>
+                </div>
+                <button class="button-add-cart" id="buttonAddCart">Adicionar ao carrinho<i class="fa-solid fa-cart-plus fa-xl"></i></button>        
+            </div>
+        `;
+        const amountProducts = document.getElementById("amountProducts");
+        let viewAmount = document.querySelector("#viewAmount");
+        let amountProductsNumber = 1;
+        const plusProducts = document.getElementById("plusProducts").addEventListener("click", () => {
+            viewAmount.value = ++amountProductsNumber;
+        });
+        const minusProducts = document.getElementById("minusProducts").addEventListener("click", () => {
+            if (viewAmount.value > 1) {
+                viewAmount.value = --amountProductsNumber;
+            };
+        }); 
+        let icon = e.parentElement;
+        icon = icon.querySelector("i");
+        ButtonAddCartFunction(icon);
+        let clickExitProduct = document.getElementById("clickExitProduct").addEventListener("click", () => {
+            if (screen.width >= 720) {
+                barPrincipal.style.display = "inline-flex";
+            } else {
+                barPrincipal.style.display = "block";
+            };
+            menuSmallView.style.display = "block";
+            productsViewInfo.style.display = "none";
+            contentSalesProducts.style.filter = "blur(0px)";
+            blurBackground.style.display = "none";
+        });
+    });
+});
